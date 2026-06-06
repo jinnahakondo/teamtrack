0e9bdf9d8650fab6eb8daad1ae9f7e0f548c4017
mdjinnahakondo <mdjinnahakondo@gmail.com>
2026-06-06 07:45:35 +0600

    added auth middleware for verify user and user role

---
+++ lib/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export type SessionUser = {
+    id: string;
+    name: string;
+    email: string;
+    role: "admin" | "project_manager" | "team_member";
+};
+
+export const verifyRole = async (...roles: SessionUser["role"][]) => {
+    const session = await getServerSession(authOptions);
+    console.log("session:", session);
+
+    // 1. if user is not logged in (401 Unauthorized)
+    if (!session?.user) {
+        return {
+            authorized: false,
+            response: NextResponse.json({ error: "Authentication required" }, { status: 401 }),
+        };
+    }
+
+    const user = session.user as SessionUser;
+
+    // 2. if user does not have the required role (403 Forbidden)
+    if (!roles.includes(user.role)) {
+        return {
+            authorized: false,
+            response: NextResponse.json({ error: "You do not have permission to access this resource" }, { status: 403 }),
+        };
+    }
+
+    // 3. if user is authorized, return the user object
+    return { authorized: true, user };
+};
